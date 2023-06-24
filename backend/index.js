fdf904025a2b2ffe4bc4cd5d45088393a192d804
Prins Naval Nuzeren <100395775+prinsereen@users.noreply.github.com>
2023-06-24 22:29:43 +0700

    Semoga Ini Beneran Selesai

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,6 @@ import UnverifiedFileRoute from "./routes/FileRoute.js";
 import AuthRoute from "./routes/AuthRoute.js";
 import SummaryRoute from "./routes/SummaryRoute.js";
 import LeeaderBoardRoute from "./routes/LeaderBoardRoute.js";
-import { Configuration, OpenAIApi} from "openai";
 
 dotenv.config();
 
@@ -51,32 +50,6 @@ app.use(AuthRoute);
 app.use(SummaryRoute);
 app.use(LeeaderBoardRoute);
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-app.post("/chatGpt", async (req, res) => {
-  try {
-    const prompt = req.body;
-
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt:  `${prompt}`,
-      temperature: 1,
-      max_tokens: 1000,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    });
-
-    res.status(200).send({
-       text : response.data.choices[0].text, 
-      });
-  } catch (error) {
-    res.status(500).json({ error: "An error occurred" });
-  }
-});
 
 /* store.sync() */
 
