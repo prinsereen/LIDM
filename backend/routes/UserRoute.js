2eeac5e31e0310f1038cd80b5da56618ca2d32c2
Hanrocky <hanrocky.halim@gmail.com>
2023-06-26 01:02:57 +0700

    cors

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -6,12 +6,12 @@ import {
   updateUser,
   deleteUser,
   updateRecomendation,
-  getUserPhoto
+  getUserPhoto,
 } from "../controllers/Users.js";
 import { verifyUser, adminOnly } from "../middleware/AuthUser.js";
 import multer from "multer";
 import path from "path";
-import cors from "cors"
+import cors from "cors";
 
 const router = express.Router();
 
@@ -38,7 +38,13 @@ const upload = multer({ storage: storage });
 router.get("/users", verifyUser, adminOnly, getUser);
 router.get("/users/:id", verifyUser, getUserById);
 router.post("/users", createUser);
-router.patch("/users/:id", cors(),  upload.single("user_photo"), updateUser);
+router.patch(
+  "/users/:id",
+  cors(),
+  verifyUser,
+  upload.single("user_photo"),
+  updateUser
+);
 router.patch("/userrec/:id", cors(), verifyUser, updateRecomendation);
 router.delete("/users/:id", verifyUser, adminOnly, deleteUser);
 router.get("/userphoto/:id", verifyUser, getUserPhoto);
