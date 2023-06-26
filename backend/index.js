fad40cb0a62366c516ee74f29dffee581285c6a0
Hanrocky <hanrocky.halim@gmail.com>
2023-06-26 11:26:03 +0700

    cors 1

--- backend/index.js
+++ backend/index.js
@@ -41,13 +41,25 @@ const store = new sessionStore({
 // );
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://literatur-production.up.railway.app");
+  const allowedOrigins = [
+    "http://localhost:5000",
+    "http://localhost:5173",
+    "https://literatur-production.up.railway.app"
+  ];
+
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  res.header("Access-Control-Allow-Credentials", "true"); // If you are using credentials
+  res.header("Access-Control-Allow-Credentials", "true");
 
   next();
 });
+
 
 // router.get("/", (req, res) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
