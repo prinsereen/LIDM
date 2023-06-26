030c75d64d745977193463e79647445f262d9410
Hanrocky <hanrocky.halim@gmail.com>
2023-06-26 17:10:01 +0700

    redirect

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { store } from "./app/store.js";
@@ -9,11 +9,6 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
-const handleBeforeUnload = () => {
-  // Save the current page URL in localStorage
-  localStorage.setItem("previousPage", window.location.href);
-};
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <Provider store={store}>
@@ -21,6 +16,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   </Provider>
   // </React.StrictMode>
 );
-
-// Listen for the beforeunload event to save the current page URL
-window.addEventListener("beforeunload", handleBeforeUnload);
