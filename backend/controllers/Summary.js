21fd371e6b0b2848f0486bc8290098b1cc6b5287
Hanrocky <hanrocky.halim@gmail.com>
2023-07-04 21:56:30 +0700

    ai detection

--- backend/controllers/Summary.js
+++ backend/controllers/Summary.js
@@ -208,7 +208,7 @@ export const createSummary = async (req, res) => {
     let aiDetection = await axios.request(aiSimilarity);
     aiDetection = aiDetection.data.fake_probability;
     let nilaiAkhir = grade.data.prediction;
-    nilaiAkhir = nilaiAkhir - nilaiAkhir * (aiDetection * 100);
+    nilaiAkhir = nilaiAkhir - nilaiAkhir * (aiDetection * 30);
     if (nilaiAkhir < 0) {
       nilaiAkhir = 0;
     }
